7624092535a24a930e9ada5218b102ee5b43d3bc
ilvlkv <volkovia012@gmail.com>
2023-01-26 18:51:59 +0700

    added new message test function

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,18 @@
+// Импорты
+import uniqid from 'uniqid';
+import { format, isToday } from 'date-fns';
+
+// Инициализация приложения
+
+const me = uniqid();
+const my_picture = 'https://www.hallmarktour.com/img/profile-img.jpg';
+
+window.addEventListener('load', function () {
+  const profile_picture = document.querySelector('.menu__profile-link img');
+
+  profile_picture.setAttribute('src', my_picture);
+});
+
 // Всплывающее меню
 const menu_toggler = document.querySelector('.hamburger');
 const menu_block = document.querySelector('.chats-block');
@@ -72,4 +87,132 @@ function hideProfileSettingsModal() {
     modals_overlay_condition.remove('modals_exit');
   }, 500);
   modals_overlay_condition.add('modals_exit');
+}
+
+// Создание нового сообщения
+
+const message_storage = [];
+class Message {
+  constructor(options) {
+    (this.id = options.id),
+      (this.author = {
+        name: options.author.author,
+        picture: options.author.picture,
+      }),
+      (this.date = options.date),
+      (this.text = options.text);
+  }
+
+  renderMessage() {
+    const message_template = document.getElementById('message');
+    const messages_block = document.querySelector(
+      '.chat-interface__messages-block'
+    );
+
+    const this_message = message_template.content.cloneNode(true);
+    const this_message_author = this_message.querySelector(
+      '.message__author-name'
+    );
+    const this_message_author_img = this_message.querySelector(
+      '.message__author-img'
+    );
+    const this_message_text = this_message.querySelector('.message__text');
+    const this_message_date = this_message.querySelector('.message__date');
+
+    this_message_author.innerHTML = this.author.name;
+    this_message_text.innerHTML = this.text;
+    this_message_date.innerHTML = this.date;
+    this_message_author_img.setAttribute('src', this.author.picture);
+
+    if (this_message_author.textContent === me) {
+      this_message.querySelector('.message').classList.add('message_outgoing');
+      this_message_author.innerHTML = 'Вы';
+    }
+
+    setTimeout(() => {
+      messages_block.prepend(this_message);
+    }, 0);
+  }
+
+  changeLastMessageOfChat() {
+    const last_message_author_img = document.querySelector(
+      '.chat__last-message-author'
+    );
+    const last_message_text = document.querySelector(
+      '.chat__last-message-text'
+    );
+    const last_message_date = document.querySelector(
+      '.chat__last-message-date'
+    );
+
+    last_message_author_img.setAttribute('src', this.author.picture);
+    last_message_text.innerHTML = this.text;
+    last_message_date.innerHTML = this.date;
+  }
+}
+
+const new_message_input = document.getElementById('new_message_input');
+const new_message_submit = document.getElementById('new_message_submit');
+
+new_message_submit.addEventListener('click', registerNewMessage);
+
+function registerNewMessage() {
+  const text = new_message_input.value;
+  const author = me;
+  const profile_picture = my_picture;
+  let date = Date.now();
+  const id = uniqid();
+
+  if (isToday(date) === true) {
+    date = format(date, 'kk:mm');
+  } else {
+    date = format(date, 'dd MMM kk:mm');
+  }
+
+  let options = {
+    id: id,
+    author: { author: author, picture: profile_picture },
+    date: date,
+    text: text,
+  };
+
+  const new_message = new Message(options);
+
+  message_storage.push(new_message);
+
+  if (!empty_chat_warning.classList.contains('hidden')) {
+    empty_chat_warning.classList.add('hidden');
+  }
+
+  new_message_input.value = '';
+
+  new_message.renderMessage();
+  new_message.changeLastMessageOfChat();
+}
+
+// Варнинги
+
+const empty_chat_warning = document.querySelector('.empty-warning');
+
+// Выбор чата
+
+const test_chat = document.querySelector('.chat');
+
+test_chat.addEventListener('click', showThisChat);
+
+function showThisChat() {
+  test_chat.classList.add('chat_active');
+
+  const chat_interface_block = document.querySelector('.chat-interface');
+  const messages_list = document.querySelector(
+    '.chat-interface__messages-block'
+  );
+
+  if (chat_interface_block.classList.contains('hidden')) {
+    chat_interface_block.classList.remove('hidden');
+  }
+
+  if (messages_list.childNodes.length < 4) {
+    empty_chat_warning.classList.remove('hidden');
+  }
 }
